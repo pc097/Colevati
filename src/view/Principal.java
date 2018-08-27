6480ea4232fca34701e30f33b8dc82ce254fa023
Lab103 <Lab103@LAB101-04>
2018-08-27 16:04:36 -0300

    Exemplo Colevati processos

--- src/view/Principal.java
+++ src/view/Principal.java
@@ -5,12 +5,12 @@ import controller.ProcController;
 public class Principal {
 	public static void main(String[] args) {
 		ProcController pCont = new ProcController();
-		// pCont.Prop();
+		pCont.Prop();
 		// String path = "TASKLIST /FO TABLE";
 		// pCont.leProcesso(path);
 		// String path = "C:\\Windows\\cmd";
 		// pCont.chamaProcesso(path);
-		String infoProcesso = "cmd.exe";
-		pCont.mataProcesso(infoProcesso);
+		// String infoProcesso = "cmd.exe";
+		// pCont.mataProcesso(infoProcesso);
 	}
 }
